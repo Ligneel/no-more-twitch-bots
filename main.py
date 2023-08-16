f062aada5bb025ed511c0190780a4791edb4fe1d
Ligneel <g.amat.nicolas@gmail.com>
2023-08-16 15:29:44 +0200

    Update main.py
    updated to a more recent version of the twitch API using chatgpt, seems to work.
    also edited the client_ID to the one from twitchapps.com/tmi/

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from irc.bot import SingleServerIRCBot
 
 USERNAME = 'A MOD OF YOUR CHANNEL'
 CHANNEL = 'YOUR CHANNEL'
-CLIENT_ID = 'GET ONE IN https://dev.twitch.tv/'
+CLIENT_ID = 'q6batx0epp608isickayubi39itsckt'
 TOKEN = 'GET ONE IN https://twitchapps.com/tmi/'
 DEBUG = False
 WHITELIST = ['streamelemenmts', 'nightbot', 'moobot']
@@ -17,26 +17,28 @@ THRESHOLD = 300
 
 class TwitchBotBan(SingleServerIRCBot):
     def __init__(self):
-        # Get the channel id, we will need this for v5 API calls
-        url = 'https://api.twitch.tv/kraken/users?login=' + CHANNEL
-        headers = {'Client-ID': CLIENT_ID, 'Accept': 'application/vnd.twitchtv.v5+json'}
+        # Fetching channel ID
+        print("Fetching Channel ID...")
+        url = 'https://api.twitch.tv/helix/users?login=' + CHANNEL
+        headers = {'Client-ID': CLIENT_ID, 'Authorization': f'Bearer {TOKEN}'}
         r = requests.get(url, headers=headers).json()
-        self.channel_id = r['users'][0]['_id']
+        self.channel_id = r['data'][0]['id']
         self.lock = RLock()
         self.channel = CHANNEL
 
-        # Create IRC bot connection
+        # Connect to IRC
+        print("Connecting to IRC...")
         server = 'irc.chat.twitch.tv'
         port = 6667
         SingleServerIRCBot.__init__(self, [(server, port, 'oauth:' + TOKEN)], USERNAME, USERNAME)
 
     def on_welcome(self, c, e):
-        # You must request specific capabilities before you can use them
         c.cap('REQ', ':twitch.tv/membership')
         c.cap('REQ', ':twitch.tv/tags')
         c.cap('REQ', ':twitch.tv/commands')
         c.join(self.channel)
         print("Chatbot started, fetching mod users")
+
         r = requests.get('https://api.twitchinsights.net/v1/bots/all').json()
         lst = list(filter(lambda x: x[1] > THRESHOLD, r['bots']))
         for idx, entry in enumerate(lst):
@@ -56,9 +58,30 @@ class TwitchBotBan(SingleServerIRCBot):
     def unban(self, username):
         self.connection.privmsg(self.channel, "/unban " + username)
 
+    def on_disconnect(self, c, e):
+        print("Disconnected from IRC.")
 
-if DEBUG:
-    logging.basicConfig(level=logging.DEBUG)
+    def on_join(self, c, e):
+        print(f"Joined {e.target}.")
 
-print("Initializing")
-Thread(target=TwitchBotBan().start).start()
+    def on_error(self, c, e):
+        print(f"Error: {e.target}, {e.source}, {e.arguments}")
+
+if __name__ == "__main__":
+    if DEBUG:
+        logging.basicConfig(level=logging.DEBUG)
+
+    print("Initializing...")
+
+    bot = TwitchBotBan()
+
+    thread = Thread(target=bot.start)
+    thread.daemon = True
+
+    try:
+        thread.start()
+        while thread.is_alive():
+            thread.join(1)
+    except KeyboardInterrupt:
+        print("\nShutting down...")
+        sys.exit(0)
